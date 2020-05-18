5b367afcecd382fa7409dbd280f2f7286f4a405c
annashamray <anna.shamray@maykinmedia.nl>
2020-05-18 17:27:11 +0200

    generate schema

--- src/objecttypes/api/urls.py
+++ src/objecttypes/api/urls.py
@@ -1,10 +1,42 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path, re_path
 
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
 
 from .views import ObjectTypeViewSet
 
 router = routers.DefaultRouter()
 router.register(r"objecttypes", ObjectTypeViewSet)
 
-urlpatterns = [url("v1/", include([url("", include(router.urls))]),)]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Object Types API",
+        default_version="v1",
+        description="OAS for Objects Types API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("v1/", include(router.urls)),
+    path(
+        "v1/",
+        include(
+            [
+                re_path(
+                    r"^openapi(?P<format>\.json|\.yaml)$",
+                    schema_view.without_ui(cache_timeout=0),
+                    name="schema-json",
+                ),
+                re_path(
+                    r"^schema/$",
+                    schema_view.with_ui("redoc", cache_timeout=0),
+                    name="schema-redoc",
+                ),
+            ]
+        ),
+    ),
+]
