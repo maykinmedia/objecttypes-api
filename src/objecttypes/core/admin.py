a5a997c0f510337686050bbf6019ba14f21ece96
annashamray <anna.shamray@maykinmedia.nl>
2020-08-27 14:40:15 +0200

    :sparkles: Fixes https://github.com/maykinmedia/objects-api/issues/23 -- always display one version in object type admin page

--- src/objecttypes/core/admin.py
+++ src/objecttypes/core/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import ObjectType, ObjectVersion
 
 
-class ObjectVersionInline(admin.TabularInline):
+class ObjectVersionInline(admin.StackedInline):
+    verbose_name_plural = _("last version")
     model = ObjectVersion
-    extra = 1
+    extra = 0
+    max_num = 1
+    min_num = 1
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        last_version = queryset.order_by("-version").first()
+        return queryset.filter(id=last_version.id)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(ObjectType)
