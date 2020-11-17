31401571b6fd9c0a7affe4a3f2c4dc7f92387192
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-11-17 18:00:11 +0100

    :sparkles: make API endpoints writable

--- src/objecttypes/api/views.py
+++ src/objecttypes/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from vng_api_common.viewsets import NestedViewSetMixin
 
 from objecttypes.core.models import ObjectType, ObjectVersion
@@ -7,14 +7,21 @@ from .filters import ObjectTypeFilterSet
 from .serializers import ObjectTypeSerializer, ObjectVersionSerializer
 
 
-class ObjectTypeViewSet(viewsets.ReadOnlyModelViewSet):
+class ObjectTypeViewSet(
+    mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.ReadOnlyModelViewSet
+):
     queryset = ObjectType.objects.prefetch_related("versions").order_by("-pk")
     serializer_class = ObjectTypeSerializer
     lookup_field = "uuid"
     filterset_class = ObjectTypeFilterSet
 
 
-class ObjectVersionViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
+class ObjectVersionViewSet(
+    NestedViewSetMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.ReadOnlyModelViewSet,
+):
     queryset = ObjectVersion.objects.order_by("object_type", "-version")
     serializer_class = ObjectVersionSerializer
     lookup_field = "version"
