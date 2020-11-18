167a00c9d6a17173007ccaca001c862344107427
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-11-18 17:39:06 +0100

    :sparkles: add versions/<version>/publish endpoint

--- src/objecttypes/api/views.py
+++ src/objecttypes/api/views.py
@@ -1,6 +1,10 @@
+from drf_yasg.utils import no_body, swagger_auto_schema
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from vng_api_common.viewsets import NestedViewSetMixin
 
+from objecttypes.core.constants import ObjectVersionStatus
 from objecttypes.core.models import ObjectType, ObjectVersion
 
 from .filters import ObjectTypeFilterSet
@@ -25,3 +29,14 @@ class ObjectVersionViewSet(
     queryset = ObjectVersion.objects.order_by("object_type", "-version")
     serializer_class = ObjectVersionSerializer
     lookup_field = "version"
+
+    @swagger_auto_schema(request_body=no_body)
+    @action(detail=True, methods=["post"])
+    def publish(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = ObjectVersionStatus.published
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
