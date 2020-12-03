569c932857504315ba802728087015c4407c4f29
Joeri Bekker <joeri@maykinmedia.nl>
2020-12-03 12:05:56 +0100

    :bento: Updated demo data.

---
+++ bin/generate_demo_fixture.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Dump the current (local database) admin layout to a JSON fixture. This
+# overwrites the existing one.
+#
+# You can load this fixture with:
+# $ src/manage.py loaddata demodata
+#
+# Run this script from the root of the repository
+
+src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary core > src/objecttypes/fixtures/demodata.json
