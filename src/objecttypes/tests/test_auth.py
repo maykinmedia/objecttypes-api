8d90571927d47adc7cba5192590663fd26c29155
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-12-21 18:14:03 +0100

    :white_check_mark: Ref. https://github.com/maykinmedia/objects-api/issues/115 -- test refactored token auth

--- src/objecttypes/tests/test_auth.py
+++ src/objecttypes/tests/test_auth.py
@@ -4,18 +4,37 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from objecttypes.core.tests.factories import ObjectTypeFactory
+from objecttypes.token.models import TokenAuth
 
 
 class AuthTests(APITestCase):
-    def test_non_auth(self):
+    def setUp(self) -> None:
         object_type = ObjectTypeFactory.create()
-
-        urls = [
+        self.urls = [
             reverse("objecttype-list"),
             reverse("objecttype-detail", args=[object_type.uuid]),
         ]
 
-        for url in urls:
+    def test_non_auth(self):
+        for url in self.urls:
             with self.subTest(url=url):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_invalid_token(self):
+        TokenAuth.objects.create(contact_person="John Smith", email="smith@bomen.nl")
+        for url in self.urls:
+            with self.subTest(url=url):
+                response = self.client.get(url, HTTP_AUTHORIZATION=f"Token 12345")
+                self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_valid_token(self):
+        token_auth = TokenAuth.objects.create(
+            contact_person="John Smith", email="smith@bomen.nl"
+        )
+        for url in self.urls:
+            with self.subTest(url=url):
+                response = self.client.get(
+                    url, HTTP_AUTHORIZATION=f"Token {token_auth.token}"
+                )
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
