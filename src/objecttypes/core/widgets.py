f1612143d3461d307ddd3c4eef5b804a7a5a099d
Steven Bal <steven@maykinmedia.nl>
2021-09-27 12:57:00 +0200

    :bug: Fix JSONSuit widget for invalid JSON values
    fixes: https://github.com/maykinmedia/objects-api/issues/253

---
+++ src/objecttypes/core/widgets.py
@@ -0,0 +1,16 @@
+import json
+
+from jsonsuit.widgets import JSONSuit as _JSONSuit
+
+
+class JSONSuit(_JSONSuit):
+    initial = dict()
+
+    def render(self, name, value, attrs={}, renderer=None):
+        try:
+            json.loads(value)
+        except ValueError:
+            # The supplied value is not valid JSON, use the original value as
+            # a fallback
+            value = json.dumps(self.initial)
+        return super().render(name, value, attrs)
