85598f20cb116dae0e0447c3db9ec9bfc312294f
Viicos <65306057+Viicos@users.noreply.github.com>
2024-02-14 11:24:41 +0100

    Replace drepecated `uget*` calls

--- src/objecttypes/token/models.py
+++ src/objecttypes/token/models.py
@@ -2,7 +2,7 @@ import binascii
 import os
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class TokenAuth(models.Model):
