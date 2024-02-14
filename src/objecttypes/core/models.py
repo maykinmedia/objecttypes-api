ab5cb7210a0c42e9248dce72dea0029a29a2479a
Viicos <65306057+Viicos@users.noreply.github.com>
2024-02-14 11:24:41 +0100

    Replace drepecated `uget*` calls

--- src/objecttypes/core/models.py
+++ src/objecttypes/core/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import date
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .constants import (
     DataClassificationChoices,
