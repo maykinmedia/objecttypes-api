dc08f1c807514057b0a8ca18b51941523b280d27
Steven Bal <steven@maykinmedia.nl>
2024-05-01 12:17:28 +0200

    :wrench: [open-zaak/open-zaak#1629] Refactor settings module
    use base settings from open-api-framework and add staging/production settings

---
+++ src/objecttypes/conf/staging.py
@@ -0,0 +1,10 @@
+"""
+Staging environment settings module.
+
+This *should* be nearly identical to production.
+"""
+import os
+
+os.environ.setdefault("ENVIRONMENT", "staging")
+
+from .production import *  # noqa
