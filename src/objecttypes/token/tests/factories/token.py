b596a135528fe2a609902f1444f85e9a2113b57a
Daniel Mursa <daniel@maykinmedia.nl>
2024-12-06 11:37:12 +0100

    [maykinmedia/objects-api#481] Add factory

---
+++ src/objecttypes/token/tests/factories/token.py
@@ -0,0 +1,11 @@
+import factory
+from objecttypes.token.models import TokenAuth
+
+
+class TokenAuthFactory(factory.django.DjangoModelFactory):
+    identifier = factory.Sequence(lambda sequence: f"token-{sequence}")
+    contact_person = factory.Faker("name")
+    email = factory.Faker("email")
+
+    class Meta:
+        model = TokenAuth
