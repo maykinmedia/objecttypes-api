dd87a6324c097efeb7c425d561cf81df01b00a84
Daniel Mursa <daniel@maykinmedia.nl>
2024-12-09 14:23:49 +0100

    [maykinmedia/objects-api#481] Fix setup_configuration

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -7,7 +7,7 @@ set -e
 
 if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
     # wait for required services
-    /wait_for_db.sh
+    ${SCRIPTPATH}/wait_for_db.sh
 
     src/manage.py migrate
     src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
