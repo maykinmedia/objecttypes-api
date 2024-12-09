b94ee295fcad34835f97e11c4139ce597a47782f
Daniel Mursa <daniel@maykinmedia.nl>
2024-12-09 09:44:19 +0100

    [maykinmedia/objects-api#481] Update command line

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -3,9 +3,12 @@
 # setup initial configuration using environment variables
 # Run this script from the root of the repository
 
-#set -e
-${SCRIPTPATH}/wait_for_db.sh
+set -e
 
-src/manage.py migrate
+if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
+    # wait for required services
+    /wait_for_db.sh
 
-src/manage.py setup_configuration --no-selftest
+    src/manage.py migrate
+    src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
+fi
