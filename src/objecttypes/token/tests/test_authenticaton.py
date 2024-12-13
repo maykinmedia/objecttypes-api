3310deb7875aca0e516a95329d1a62ec14b52efe
Daniel Mursa <daniel@maykinmedia.nl>
2024-12-13 12:01:03 +0100

    [maykinmedia/objects-api#481] Fix token tests

--- src/objecttypes/token/tests/test_authenticaton.py
+++ src/objecttypes/token/tests/test_authenticaton.py
@@ -19,16 +19,42 @@ class TestTokenAuthAuthorization(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_invalid_token(self):
+    def test_valid_token_with_no_spaces(self):
+        token_auth = TokenAuth.objects.create(
+            contact_person="contact_person",
+            email="contact_person@gmail.nl",
+            identifier="token-1",
+            token="1234-Token-5678",
+        )
+        response = self.client.get(
+            reverse("v2:objecttype-list"),
+            HTTP_AUTHORIZATION=f"Token {token_auth.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_token_with_spaces(self):
+        token_auth = TokenAuth.objects.create(
+            contact_person="contact_person",
+            email="contact_person@gmail.nl",
+            identifier="token-1",
+            token="1234 Token 5678",
+        )
+        response = self.client.get(
+            reverse("v2:objecttype-list"),
+            HTTP_AUTHORIZATION=f"Token {token_auth.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_invalid_token_existing(self):
         response = self.client.get(
             reverse("v2:objecttype-list"),
             HTTP_AUTHORIZATION="Token 1234-Token-5678",
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
+    
     def test_empty_token(self):
         response = self.client.get(
-            reverse("v2:objecttype-list"), HTTP_AUTHORIZATION="Token"
+            reverse("v2:objecttype-list"), HTTP_AUTHORIZATION="Token "
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
