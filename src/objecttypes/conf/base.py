e8d22de375dfffd6148ab29311211cf0f634d39a
danielmursa-dev <daniel@maykinmedia.nl>
2024-12-17 11:38:04 +0100

    Issue/update latest versions (#137)
    * Update requirements
    
    * Update settings and fix tests

--- src/objecttypes/conf/base.py
+++ src/objecttypes/conf/base.py
@@ -53,5 +53,6 @@ ADMIN_INDEX_DISPLAY_DROP_DOWN_MENU_CONDITION_FUNCTION = (
 #
 SETUP_CONFIGURATION_STEPS = [
     "objecttypes.setup_configuration.steps.token_auth.TokenAuthConfigurationStep",
+    "django_setup_configuration.contrib.sites.steps.SitesConfigurationStep",
     "mozilla_django_oidc_db.setup_configuration.steps.AdminOIDCConfigurationStep",
 ]
