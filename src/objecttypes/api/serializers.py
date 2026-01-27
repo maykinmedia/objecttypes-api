71ba4fecd4df6bbe112e9e292f616ab221b13e3e
Chris Wesseling <chris@maykinmedia.nl>
2026-01-27 11:45:32 +0100

    Revert "Merge pull request #215 from maykinmedia/add-ot-linkable-to-zaken-flag"
    This reverts commit fec7bdbef48c8da72d5be3fcb4d96505a2a1ed10, reversing
    changes made to 677299e835e6191600cdf70229bc33ddc5e6bebc.

--- src/objecttypes/api/serializers.py
+++ src/objecttypes/api/serializers.py
@@ -110,7 +110,6 @@ class ObjectTypeSerializer(serializers.HyperlinkedModelSerializer):
             "providerOrganization",
             "documentationUrl",
             "labels",
-            "linkableToZaken",
             "createdAt",
             "modifiedAt",
             "allowGeometry",
@@ -129,7 +128,6 @@ class ObjectTypeSerializer(serializers.HyperlinkedModelSerializer):
             "providerOrganization": {"source": "provider_organization"},
             "documentationUrl": {"source": "documentation_url"},
             "allowGeometry": {"source": "allow_geometry"},
-            "linkableToZaken": {"source": "linkable_to_zaken"},
             "createdAt": {"source": "created_at", "read_only": True},
             "modifiedAt": {"source": "modified_at", "read_only": True},
         }
